55ff14b7b4191be6de79e58ea66f3c1ffa1c625a
Ahmad Barqawi <Barqawi.88@outlook.com>
2024-02-14 10:07:42 +0000

    update the versions

--- setup.py
+++ setup.py
@@ -23,6 +23,6 @@ setup(
     description="Detect faces and draw overlay images",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    install_requires=[ 'opencv-python==3.4.18.65', 'Pillow==9.4.0', 'tensorflow==2.9.0', 'keras==2.9.0', 'numpy==1.24.1', 'Markdown==3.4.1', 'absl-py==1.4.0', 'Werkzeug==2.2.2', 'h5py==3.1.0', 'intelli==0.0.8'    ],
+    install_requires=[ 'opencv-python==3.4.18.65', 'Pillow==9.4.0', 'tensorflow==2.9.0', 'keras==2.9.0', 'numpy==1.24.1', 'Markdown==3.4.1', 'absl-py==1.4.0', 'Werkzeug==2.2.2', 'h5py==3.1.0', 'intelli==0.0.8'],
     data_files=[('snappy2/resource', resources)]
 )
